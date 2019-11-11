05558b7fbae55c1c91de496e112b93d06ec04a29
unknown <jonathan.sammons1005@gmail.com>
2019-11-11 12:18:10 +0000

    Pushing event updates

---
+++ backend/src/controllers/UpdateEventController.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const Event = require('../models/Event');
+const User = require('../models/User');
+
+module.exports = {
+    async updateEvent(req, res){ 
+        const { id, eventName, game, location, numOfPlayers, eventDate, duration } = req.body;   
+        let eventFromDB = await Event.findOne({ _id: id });        
+        if(!eventFromDB) return res.status(404).send({Message: 'Unable to locate an event with that ID.'});
+        else{                 
+            try{
+                if(eventFromDB.eventName !== eventName) eventFromDB.eventName = eventName;
+                if(eventFromDB.game !== game) eventFromDB.game = game;
+                if(eventFromDB.location !== location) eventFromDB.location = location;
+                if(eventFromDB.numOfPlayers !== numOfPlayers) eventFromDB.numOfPlayers = numOfPlayers;
+                if(eventFromDB.eventDate !== eventDate) eventFromDB.eventDate = eventDate;
+                if(eventFromDB.duration !== duration) eventFromDB.duration = duration;
+                
+                await eventFromDB.save();        
+                res.status(200).json({Message: 'Event Updated Successfully!'});
+            }catch{
+                res.status(500).send();
+            }
+        }               
+    }
+
+}
