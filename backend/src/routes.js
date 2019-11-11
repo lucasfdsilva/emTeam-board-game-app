3fed546e15e8703a359eae77890182c3b78aef6b
unknown <jonathan.sammons1005@gmail.com>
2019-11-11 12:15:05 +0000

    Added GET, UPDATE, and DELETE Event Routes and controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,6 +9,10 @@ const LoginController = require('./controllers/LoginController');
 const ForgotPasswordController = require('./controllers/ForgotPasswordController');
 const ResetPasswordController = require('./controllers/ResetPasswordController');
 const CreateEventController = require('./controllers/CreateEventController');
+const UpdateEventController = require('./controllers/UpdateEventController');
+const GetAllEventsController = require('./controllers/GetAllEventsController');
+const GetEventController = require('./controllers/GetEventController');
+const DeleteEventController = require('./controllers/DeleteEventController');
 
 //Declaring Router Instances
 const openRoutes = express.Router();
@@ -22,6 +26,10 @@ openRoutes.post('/users/login', LoginController.login);
 openRoutes.post('/users/forgot_password', ForgotPasswordController.forgotPassword);
 openRoutes.post('/users/reset_password', ResetPasswordController.reset);
 openRoutes.post('/events/create', CreateEventController.createEvent);
+openRoutes.put('/events/update', UpdateEventController.updateEvent);
+openRoutes.get('/events', GetAllEventsController.getEvents);
+openRoutes.get('/events/id', GetEventController.getEvent);
+openRoutes.delete('/events/delete', DeleteEventController.deleteEvent);
 
 
 module.exports = {
