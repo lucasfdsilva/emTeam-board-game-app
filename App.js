42070dea2b7888e6a784710fe4f29c67679bef83
Serhazor <44921903+Serhazor@users.noreply.github.com>
2019-11-14 14:05:19 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, Header } from 'react-navigation-stack';
 import HomeScreen from './src/screens/HomeScreen';
 import GameScreen from './src/screens/GameScreen';
 import Routes from './src/Routes';
@@ -12,9 +12,9 @@ routes: Routes
 }, {
 initialRouteName: 'routes', 
 defaultNavigationOptions:{
-  title: 'emGame Finder'
+  title: 'emGame Finder',
+  header: null, //this is to hide the navigation bar as it's partially blocking hte logo
 }
 });
-
 
 export default createAppContainer(navigation); //created as we need to always display somethign to the screen and alwasy export some component
