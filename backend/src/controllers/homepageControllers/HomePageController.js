eef1f9586fb4efb7af67a01ca74afe50e76d2aca
LucDSilva <lucas.silva00@outlook.ie>
2019-11-15 19:54:32 +0000

    added additional tests

--- backend/src/controllers/homepageControllers/HomePageController.js
+++ backend/src/controllers/homepageControllers/HomePageController.js
@@ -4,7 +4,7 @@ module.exports = {
 
     homePage(req, res){
 
-        res.send('Welcome to Board Geek API');
+        res.send('Welcome to Board Geek API. To know more about this API, please visit https://github.com/LucDSilva/emTeam-board-game-app');
     
     }
 
