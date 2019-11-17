a1a06bd74da149e61ea4e1a57ebbf2ebb6c51bc5
LucDSilva <lucas.silva00@outlook.ie>
2019-11-17 22:03:16 +0000

    adding extra functionality to Home Page

--- backend/src/controllers/homepageControllers/HomePageController.js
+++ backend/src/controllers/homepageControllers/HomePageController.js
@@ -1,10 +1,9 @@
 
 module.exports = {
 
+    async homePage(req, res){
 
-    homePage(req, res){
-
-        res.send('Welcome to Board Geek API. To know more about this API, please visit https://github.com/LucDSilva/emTeam-board-game-app');
+        res.send('Hi. Welcome to Board Geek API. To know more about this API, please visit https://github.com/LucDSilva/emTeam-board-game-app');
     
     }
 
