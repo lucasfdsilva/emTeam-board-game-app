d0b1458ff15ec7327bfc7f7c73e659437a90c260
LucDSilva <lucas.silva00@outlook.ie>
2019-11-19 16:15:25 +0000

    updated home page

--- backend/src/controllers/homepageControllers/HomePageController.js
+++ backend/src/controllers/homepageControllers/HomePageController.js
@@ -3,7 +3,7 @@ module.exports = {
 
     async homePage(req, res){
 
-        res.send('Hi. Welcome to Board Geek API. To know more about this API, please visit https://github.com/LucDSilva/emTeam-board-game-app');
+        res.send(`<h1>Hi, Welcome to Emteam's Board Geek API</h1> <h2>To know more about this API, please visit our <a href="https://github.com/LucDSilva/emTeam-board-game-app">GitHub Repo</h2>`);
     
     }
 
