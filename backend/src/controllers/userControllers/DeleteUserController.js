c2e7b19c754d35ae4be5619dceb8298433c81f1a
LucDSilva <lucas.silva00@outlook.ie>
2019-12-03 10:53:04 +0000

    updated delete user method to remove player from events

--- backend/src/controllers/userControllers/DeleteUserController.js
+++ backend/src/controllers/userControllers/DeleteUserController.js
@@ -1,4 +1,5 @@
 const User = require('../../models/User');
+const Event = require('../../models/Event');
 
 module.exports = {
 
@@ -17,6 +18,21 @@ module.exports = {
             if (!userFromDB) {
                 return res.status(400).json({ message: "User Not Found" });
             } else {
+ 
+                for (event of userFromDB.joinedEvents) {
+                    let eventFromDB = await Event.findOne({ _id: event });
+
+                    if(eventFromDB){ 
+                        await eventFromDB.participants.forEach((participantUserId, index) => {
+                            if(participantUserId == id){
+                                eventFromDB.participants.splice(index, 1);
+                                eventFromDB.save();
+                                console.log('User removed from event'); 
+                            }
+                        });
+                    }
+                }//End of loop through joined events
+
                 await userFromDB.remove();
 
                 res.status(200).json({ message: 'User Account deleted succesfully' });
