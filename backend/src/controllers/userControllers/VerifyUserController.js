2c3e060a7aff62a8683b18a770a8cfc6750ed130
lucasfdsilva <lucas.silva00@outlook.ie>
2020-05-10 23:08:08 +0100

    update

--- backend/src/controllers/userControllers/VerifyUserController.js
+++ backend/src/controllers/userControllers/VerifyUserController.js
@@ -20,7 +20,7 @@ module.exports = {
 
         await userFromDB.save();
 
-        res.status(200).json({ message: "Success: User email verified succesfully" });
+        res.status(200).json({ message: "Congratulations: User email verified succesfully" });
 
       }
     } catch {
